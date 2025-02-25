6a2525763cba03531a1a4a1642653d560742e19a
Wallace Henrique <144370869+wollihenrique@users.noreply.github.com>
2025-02-25 16:22:43 -0300

    Correção de erro

--- test.php
+++ test.php
@@ -8,10 +8,10 @@ require_once __DIR__ . '/autoload.php';
 $Wallace = new ClassPerson("Wallace Henrique Batista Santos", 1.79, new \DateTimeImmutable("2007-11-11"));
 echo "Nome: " . $Wallace->getName() . PHP_EOL . "Altura: " . $Wallace->getHeight() . PHP_EOL . "Idade: " . $Wallace->age() . " anos"; 
 
-$Mirielle = new BankAccount(titular: "Mirielle de Souza Silva", saldo: 1.00);
-echo PHP_EOL . "Saldo: " . $Mirielle->getSaldo();
-$Mirielle->depositar(150.50);
-$Mirielle->sacar(200);
-echo PHP_EOL . "Saldo: " . $Mirielle->getSaldo() . "\n";
+$pessoa = new BankAccount(titular: "pessoa de Souza Silva", saldo: 1.00);
+echo PHP_EOL . "Saldo: " . $pessoa->getSaldo();
+$pessoa->depositar(150.50);
+$pessoa->sacar(200);
+echo PHP_EOL . "Saldo: " . $pessoa->getSaldo() . "\n";
 
 
